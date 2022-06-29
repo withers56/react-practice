55a73314e18d8f0fdb4622316926396e2e6a67e3
withers56 <william.k.withers@gmail.com>
2022-06-29 15:50:35 -0500

    after creating and submitting new meetup, you return back to the all meetups page

--- tutorial-app/src/pages/NewMeetups.js
+++ tutorial-app/src/pages/NewMeetups.js
@@ -1,8 +1,20 @@
+import {useHistory} from 'react-router-dom'
 import NewMeetupForm from "../componets/meetups/NewMeetupForm";
 
 function NewMeetupsPage() {
-    function addMeetupHandler(meetupData) {
+    const history = useHistory();
 
+    function addMeetupHandler(meetupData) {
+        const requestObject = {
+            method: 'POST',
+            body: JSON.stringify(meetupData),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+        fetch('https://react-practice-c1b93-default-rtdb.firebaseio.com/meetups.json', requestObject).then(function (){
+            history.replace('/')
+        });
     }
     return(
         <section>
