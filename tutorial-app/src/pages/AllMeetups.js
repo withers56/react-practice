92d19430af2e1e7ccd04aaa1fe27eb1f9cc1744f
withers56 <william.k.withers@gmail.com>
2022-06-29 16:23:31 -0500

    all meetups now sends a fetch request to firebase mock server to get meetups, using useEffect to prevent an infinite loop. ...data[keys] grabs all of that objects key value pairs, then we store those into a meetup object that gets pushed into a meetups array

--- tutorial-app/src/pages/AllMeetups.js
+++ tutorial-app/src/pages/AllMeetups.js
@@ -1,32 +1,44 @@
+import {useState, useEffect} from 'react';
+
 import MeetupsList from "../componets/meetups/MeetupsList";
 
-
-const DUMMY_DATA = [
-    {
-        id: 'm1',
-        title: 'This is a first meetup',
-        image:
-            'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
-        address: 'Meetupstreet 5, 12345 Meetup City',
-        description:
-            'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
-    },
-    {
-        id: 'm2',
-        title: 'This is a second meetup',
-        image:
-            'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
-        address: 'Meetupstreet 5, 12345 Meetup City',
-        description:
-            'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
-    },
-];
-
 function AllMeetupsPage() {
+    const [isLoading, setIsLoading] = useState(true)
+    const [loadedMeetups, setLoadedMeetups] = useState([]);
+
+    useEffect(() => {
+        setIsLoading(true);
+        fetch('https://react-practice-c1b93-default-rtdb.firebaseio.com/meetups.json').then(function (resp){
+            return resp.json();
+        }).then(function (data){
+            const meetups = [];
+
+            for (const keys in data) {
+                const meetup = {
+                    id: keys,
+                    ...data[keys]
+                };
+
+                meetups.push(meetup);
+            }
+
+            setIsLoading(false);
+            setLoadedMeetups(meetups)
+        });
+    }, [])
+
+    if (isLoading) {
+        return (
+            <section>
+                <p>Loading...</p>
+            </section>
+        )
+    }
+
     return(
         <section>
             <h1>All Meetups</h1>
-            <MeetupsList meetups={DUMMY_DATA} />\
+            <MeetupsList meetups={loadedMeetups} />
 
         </section>
     )
