70c708974043c68d5e68b0522dbf101fb7d0a4fe
withers56 <william.k.withers@gmail.com>
2022-06-30 14:57:07 -0500

    imported favoritescontextprovider and wrapped ecerything in it so that anything within it has access to its state of favorites

--- tutorial-app/src/index.js
+++ tutorial-app/src/index.js
@@ -3,12 +3,15 @@ import {BrowserRouter} from 'react-router-dom'
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {FavoritesContextProvider} from "./store/favorites-context";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>,
+    <FavoritesContextProvider>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </FavoritesContextProvider>,
 );
 
 // If you want to start measuring performance in your app, pass a function
--- tutorial-app/src/store/favorites-context.js
+++ tutorial-app/src/store/favorites-context.js
@@ -2,10 +2,15 @@ import {createContext, useState} from "react";
 
 const FavoritesContext = createContext({
     favorites: [],
-    totalFavorites: 0
+    totalFavorites: 0,
+    //initailze empty pointers to functions, pass them a paramater for IDE autocompletion, not necessaruy
+    addFavorite: (favoriteMeetup) => {},
+    removeFavorite: (meetupId) => {},
+    itemIsFavorite: (meetupId) => {}
+
 });
 
-function FavoritesContextProvider(props) {
+export function FavoritesContextProvider(props) {
     const [userFavorites, setUserFavorites] = useState([])
 
 
@@ -18,20 +23,25 @@ function FavoritesContextProvider(props) {
 
     function removeFavoritesHandler(meetupId){
         setUserFavorites(prevUserFavorites => {
-            return prevUserFavorites.filter(meetup => meetup.id != meetupId)
+            return prevUserFavorites.filter(meetup => meetup.id !== meetupId)
         })
     }
 
-    function itemIsFavoriteHandler() {
-
+    function itemIsFavoriteHandler(meetupId) {
+        return userFavorites.some(meetup => meetup.id === meetupId)
     }
 
     const context = {
         favorites: userFavorites,
-        totalFavorites: userFavorites.length
+        totalFavorites: userFavorites.length,
+        addFavorite: addFavoritesHandler,
+        removeFavorite: removeFavoritesHandler,
+        itemIsFavorite: itemIsFavoriteHandler
     }
 
     return <FavoritesContext.Provider value={context}>
         {props.children}
     </FavoritesContext.Provider>
 }
+
+export default FavoritesContext;
